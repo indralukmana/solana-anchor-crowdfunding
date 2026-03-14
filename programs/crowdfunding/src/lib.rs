3bbb83063ca44536b49135dfa6750582b40aa415
Indra Lukmana <indra@indralukmana.com>
2026-03-14 18:36:39 +0700

    feat: ✨ implement create_campaign functionality with error handling

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,11 +1,9 @@
-pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod state;
 
 use anchor_lang::prelude::*;
 
-pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
@@ -15,7 +13,7 @@ declare_id!("HysEQZD4Uz2v97vpPrysELouuHCMpmn98YwKfJ9u77XD");
 pub mod crowdfunding {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn create_campaign(ctx: Context<CreateCampaign>, goal: u64, deadline: i64) -> Result<()> {
+        create_campaign_handler(ctx, goal, deadline)
     }
 }
---
+++ programs/crowdfunding/src/state/campaign.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Campaign {
+    pub creator: Pubkey,
+    pub goal: u64,
+    pub raised: u64,
+    pub deadline: i64,
+    pub claimed: bool,
+    pub bump: u8,
+}
+
